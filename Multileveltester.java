f1c72905acbed74d584ee28d6782dc8969ae567a
lakshmi-pavani <48121331+lakshmi-pavani@users.noreply.github.com>
2020-10-30 22:32:02 +0530

    Add files via upload

---
+++ Multileveltester.java
@@ -0,0 +1,24 @@
+class Shape {
+   public void display() {
+      System.out.println("Inside display");
+   }
+}
+class Rectangle extends Shape {
+   public void area() {
+      System.out.println("Inside area");
+   }
+}
+class Cube extends Rectangle {
+   public void volume() {
+      System.out.println("Inside volume");
+   }
+}
+class Multileveltester {
+   public static void main(String args[]) {
+      Cube cube = new Cube();
+      cube.display();
+      cube.area();
+      cube.volume();
+   }
+}
+    

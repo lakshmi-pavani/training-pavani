4e172533bcf99ddb9bc0807a1993a8feb18473be
lakshmi-pavani <48121331+lakshmi-pavani@users.noreply.github.com>
2020-11-22 16:02:52 +0530

    Add files via upload

---
+++ WithoutThread.java
@@ -0,0 +1,17 @@
+package com.motivity;
+class NoThread {
+	public void show() {
+		for(int i=0;i<=20;i++) {
+			System.out.println("i value is =" +i);
+		}
+	}
+}
+public class WithoutThread {
+	public static void main(String[] args) {
+		NoThread nt=new NoThread();
+		nt.show();
+		for(int x=0;x<=20;x++) {
+			System.out.println("x value is =" +x);
+		}
+	}
+}
